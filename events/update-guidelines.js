b344bd25fb0c060c81377cf3ef94535c60283d1b
Skillez <github@skillez.eu>
2021-01-29 05:45:40 +0100

    pre.alpha20e

--- events/update-guidelines.js
+++ events/update-guidelines.js
@@ -36,6 +36,8 @@ function checkGuidelines() {
 function updateGuidelines(guidelinesMessage) {
     mysqlBotQuery(`SELECT guildDiscordID from ${config.mysql.cert_table_name} WHERE guildDiscordID IS NOT NULL`)
         .then(results => {
+            if (!results[0]) return console.error(`update-guidelines.js:1 autoLeaver() PROTECTION - Database is empty, that might be an error.`);
+
             bot.guilds.cache.forEach(guild => {
                 const certifiedClub = results.find(record => record.guildDiscordID === guild.id);
                 if (certifiedClub) {
@@ -61,6 +63,15 @@ function updateGuidelines(guidelinesMessage) {
                                 .catch(error => console.error(`update-guidelines.js:6 updateGuidelines(🔴) ${error} for the ${guidelinesChannel.name} in '${guild.name}'`));
                         } else return console.error(`update-guidelines.js:7 updateGuidelines(⭕) Missing READ_MESSAGE_HISTORY for the #${guidelinesChannel.name} in '${guild.name}'.`);
                     } else return console.error(`update-guidelines.js:8 updateGuidelines(❌) There is no '${config.guidelines.channelName}' channel in '${guild.name}'.`);
+                } else {
+                    guild.channels.cache.find(ch => ch.name === config.guidelines.channelName).messages.fetch({ limit: 1 })
+                        .then(msgs => {
+                            const gMessage = msgs.filter(message => message.author === bot.user && message.embeds[0]).first(); // gMessage has to be sent by the bot and need to have an embed.
+                            if (gMessage) return gMessage.delete()
+                                .then(() => console.info(`update-guidelines.js:9 updateGuidelines (${guild.name}) Removed guidelines messages in non-TEA server.`))
+                                .catch(error => console.error(`update-guidelines.js:10 updateGuidelines (${guild.name}) Delete error in non-TEA server guidelines ${error}`))
+                        })
+                        .catch(error => console.error(`update-guidelines.js:11 updateGuidelines (${guild.name}) Error to fetch guidelines channel messages ${error}`));
                 }
             });
         })
