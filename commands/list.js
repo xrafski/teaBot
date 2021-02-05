57abb8ac6bbb604da1354113f7af0f14e82b185d
Skillez <github@skillez.eu>
2021-02-05 16:02:26 +0100

    pre.alpha26c

--- commands/list.js
+++ commands/list.js
@@ -1,4 +1,5 @@
 const { botReply, Discord, TEAlogo } = require("../teaBot");
+const config = require('../bot-settings.json');
 
 module.exports.help = {
     name: "list",
@@ -10,14 +11,18 @@ module.exports.help = {
 module.exports.run = async (bot, message) => {
     let clubList = '';
     bot.guilds.cache.forEach(guild => {
+
+        for (const hidden of config.certification.hiddenServers) {
+            if (guild.id === hidden.guildDiscordID) return;
+        }
+
         clubList = clubList + `\n${guild.name} • '${guild.id}'`
-        console.log(guild.name)
     });
 
     const embed_club_list = new Discord.MessageEmbed()
         .setColor('#0095ff')
         .setAuthor(`Club List`, TEAlogo)
-        .setDescription((clubList.length > 2000 ? `${clubList.slice(0, 2000)}...` : `${clubList}`))
+        .setDescription(clubList = (clubList.length > 2000 ? `${clubList.slice(0, 2000)}...` : `${clubList}`) || 'No clubs found.')
         .setThumbnail(TEAlogo)
         .setTimestamp()
     botReply(embed_club_list, message);
