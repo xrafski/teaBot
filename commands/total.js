0c90ef7c7458e9a303429ffe0f50378eb686a9b5
Skillez <github@skillez.eu>
2021-02-05 16:42:04 +0100

    pre.alpha26e

---
+++ commands/total.js
@@ -0,0 +1,15 @@
+const { botReply, getEmoji } = require("../teaBot");
+const config = require('../bot-settings.json');
+
+module.exports.help = {
+    name: "total",
+    description: "Display total amount of users on all servers.",
+    type: "botowner",
+    usage: "Type the command without any arguments."
+};
+
+module.exports.run = async (bot, message) => {
+    let memberCount = 0;
+    for (const guild of bot.guilds.cache) memberCount = memberCount + guild[1].memberCount;
+    botReply(`${getEmoji(config.TEAserverID, 'TEA')} ${memberCount} total users (non-unique)`, message);
+}
--- events/ready-event.js
+++ events/ready-event.js
@@ -6,13 +6,13 @@ bot.login(config.botToken);
 bot.on('ready', () => {
     logger('info', `\n▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬\nTrove Ethics Alliance Bot (${bot.user.tag}) has logged in!\nVersion: ${BotVersion}\n▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬`);
 
-    // Set the bot user's presence
     bot.user.setPresence({ activity: { name: ' ', type: 'WATCHING' }, status: 'idle' })
         .then(() => {
             setInterval(() => {
-                let amountOfPeople = 0;
-                bot.guilds.cache.forEach(guild => amountOfPeople = amountOfPeople + guild.memberCount);
-                bot.user.setPresence({ activity: { name: `${amountOfPeople} users 👮‍♂️`, type: 'WATCHING' }, status: 'online' });
+                let memberCount = 0;
+                for (const guild of bot.guilds.cache) memberCount = memberCount + guild[1].memberCount;
+                bot.user.setPresence({ activity: { name: `${memberCount} users 👮‍♂️`, type: 'WATCHING' }, status: 'online' });
             }, 3600000);
-        }).catch(error => logger('warn',`ready-event.js:1 () Set the bot activity`, error));
+        })
+        .catch(error => logger('error', `ready-event.js:1 () Set the bot activity`, error));
 });
