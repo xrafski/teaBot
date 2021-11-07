bf621665703ca984b4769acfbef4831f73111604
Skillez <page+github@skillez.eu>
2021-11-07 15:43:34 +0100

    1.0.65

--- pm2-tea-bot.config.js
+++ pm2-tea-bot.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  apps : [{
-    name   : "Trove Ethics Alliance Bot",
-    script : "./teaBot.js",
-    error_file : "logs/error.log",
-    out_file : "logs/output.log"
+  apps: [{
+    name: 'Trove Ethics Alliance Bot',
+    script: './teaBot.js',
+    error_file: 'logs/error.log',
+    out_file: 'logs/output.log'
   }]
-}
+};
