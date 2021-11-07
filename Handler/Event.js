16b56059aa2ad45ce31294897ceaebab4daabb4b
Skillez <page+github@skillez.eu>
2021-11-07 15:08:28 +0100

    1.0.62

--- Handler/Event.js
+++ Handler/Event.js
@@ -5,12 +5,16 @@ const logger = require('../Utilities/logger');
 const globPromise = promisify(glob);
 
 module.exports = async (client) => {
-	logger.startup(`Handler/Event.js (1) Loaded '${__filename.split('\\').slice(-2).join('/')}' Handler.`);
+	logger.startup(`Handler/Event.js (1) Loaded '${__filename.split('\\').slice(-2).join('/')}' Handler.`); // Log handler behing loaded.
 
+	// Create a new table.
 	const table = new AsciiTable('Events Loaded');
 	table.setHeading('Name', 'File location');
 
+	// Create variable with event files.
 	const eventFiles = await globPromise(`${process.cwd()}/Event/*/*.js`);
+
+	// Map through files, run client event listener and add row to a table.
 	eventFiles.map((file) => {
 		const event = require(file);
 		if (!event.name) return;
@@ -20,5 +24,7 @@ module.exports = async (client) => {
 
 		table.addRow(event.name, file.split('/').slice(-3).join('/'));
 	});
+
+	// Print to the console table results.
 	console.log(table.toString());
 };
