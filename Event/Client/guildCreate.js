b761d0d12bf7e9d29698404d3082803da1c8a166
Skillez <page+github@skillez.eu>
2021-11-07 14:54:31 +0100

    1.0.57

--- Event/Client/guildCreate.js
+++ Event/Client/guildCreate.js
@@ -9,13 +9,13 @@ module.exports = {
     * @param {Client} client
     * @param {Guild} guild
     */
-    execute(client, guild) { // CHANGEME
+    execute(client, guild) {
         // registerGuildCommands(guild, guildSlashCommandsArray)
         //     .then(res => logger.info(`Event/Client/guildCreate.js (1) '${client.user.username}' just joined '${guild.name}' server. ${res}`))
         //     .catch(error => logger.log(`Event/Client/guildCreate.js (2) '${client.user.username}' just joined '${guild.name}' server with an error to register slash commands.`, error));
 
         logger.info(`Event/Client/guildCreate.js (1) '${client.user.username}' just joined '${guild.name}' server.`);
-        guild.systemChannel?.send(`Hello, I'm ${getEmoji(client.config.TEAserver.id, 'TEA')} Trove Ethics Alliance Bot.\nThanks for inviting me, type **${client.config.bot.prefix}help** to check out my commands!\nIf you need any further information or want to report bugs, please do not hesitate to contact the bot owner\n(Discord: **${client.config.bot.owner.tag}** | Trove: **RNG**).`)
-            .catch(error => logger.log('Event/Client/guildCreate.js (3) Error to send \'systemChannel\' message.', error));
+        guild.systemChannel?.send(`Hello, I'm ${getEmoji(client.config.TEAserver.id, 'TEA')} Trove Ethics Alliance Bot.\nThanks for inviting me\nIf you need any further information or want to report bugs, please do not hesitate to contact the bot owner\n(Discord: **${client.config.bot.owner.tag}**).`)
+            .catch(error => logger.log('Event/Client/guildCreate.js (3) Error to send \'systemChannel\' message', error));
     },
 };
