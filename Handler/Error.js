7633afbf51258f1f2febc462afa3cb23d0a9dc52
Skillez <page+github@skillez.eu>
2021-11-07 15:05:57 +0100

    1.0.61

--- Handler/Error.js
+++ Handler/Error.js
@@ -1,7 +1,9 @@
 const logger = require('../Utilities/logger');
 
 module.exports = async () => {
-	logger.startup(`Handler/Command.js (1) Loaded '${__filename.split('\\').slice(-2).join('/')}' Handler.`);
+	logger.startup(`Handler/Command.js (1) Loaded '${__filename.split('\\').slice(-2).join('/')}' Handler.`); // Log handler being loaded.
+
+	// Catch all unhandled rejection messages
 	process.on('unhandledRejection', (error) => {
 		console.warn('[teaBot] Unhandled promise rejection:', error);
 	});
