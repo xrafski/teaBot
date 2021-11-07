9a39d3aa70f22b0a36e93771a0efdff9e3bf464e
Skillez <page+github@skillez.eu>
2021-11-07 14:40:47 +0100

    1.0.49

--- Command/Slash/Guild/test-guild.js
+++ Command/Slash/Guild/test-guild.js
@@ -24,19 +24,20 @@ module.exports = {
 
     execute(client, interaction, args) {
         const { user, guild } = interaction;
-        logger.command(`${__filename.split('\\').slice(-4).join('/')} used by '${user?.tag}' in the '${guild?.name}' guild.`);
+        logger.command(`${__filename.split('\\').slice(-4).join('/')} used by '${user?.tag}' in the '${guild?.name}' guild.`); // Log who used the command.
 
+        // Create embed object.
         const testEmbed = new MessageEmbed()
             .setAuthor('RNG')
             .setDescription('Some description for this embed')
             .setColor('YELLOW')
             .setTitle('Embed title');
-        console.log(testEmbed);
 
-
+        // Send interaction reply with results
         interaction.reply({
             content: `${getEmoji(client.config.TEAserver.id, 'TEA')} Response correctly handled!\nArguments: ${args.join(' | ')}\nAPI Latency is **${Math.round(client.ws.ping)}** ms.}`,
             embeds: [testEmbed]
-        }).catch(console.error);
+        })
+            .catch(err => logger.log('Command/Slash/Guild/test-guild.js (1) Error to send interaction reply', err)); // Catch interaction reply error.
     }
 };
