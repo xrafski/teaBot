1bd3b111c83a616a3e23bd5c74808800d67555b6
Skillez <page+github@skillez.eu>
2021-11-07 15:00:14 +0100

    1.0.59

--- Event/Interactions/interactionCreate.js
+++ Event/Interactions/interactionCreate.js
@@ -11,11 +11,16 @@ module.exports = {
         if (interaction.isCommand() || interaction.isContextMenu()) {
             // await interaction.deferReply({ ephemeral: ephemeralToggle(interaction.commandName) }).catch(error => logger.log('Event/Interactions/interactionCreate.js (1) Error to send deferReply', error));
 
+            // Assing variable to a command.
             const command = client.slashCommands.get(interaction.commandName);
+
+            // Check if command exists
             if (!command) return interaction.reply({ content: '⛔ An error occured while trying to execute this command.' }); // && client.slashCommands.delete(interaction.commandName);
 
+            // Create args array
             const args = [];
 
+            // Loop through interaction.options.data to get arguments into args array
             for (const option of interaction.options.data) {
                 if (option.type === 'SUB_COMMAND') {
                     if (option.name) args.push(option.name);
@@ -26,6 +31,7 @@ module.exports = {
                 else if (option.value) { args.push(option.value); }
             }
 
+            // Execute the command.
             command.execute(client, interaction, args);
         }
     },
